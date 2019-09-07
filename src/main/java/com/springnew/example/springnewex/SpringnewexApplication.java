26d9d9be15646855af2ab8de94efdfa032b00435
nagamalleswararaoj <nmrao.mtech@gmail.com>
2019-09-07 11:28:00 +0530

    intial commiting

---
+++ src/main/java/com/springnew/example/springnewex/SpringnewexApplication.java
@@ -0,0 +1,14 @@
+package com.springnew.example.springnewex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringnewexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringnewexApplication.class, args);
+		System.out.println("welcome");
+	}
+
+}
